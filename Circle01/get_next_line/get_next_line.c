656dfb1f79006af41eb1ec2604b5b5cda8ee1096
gipark <gipark@student.42seoul.kr>
2020-06-19 00:46:47 +0900

    get_next_line/0/114%[Perfect] [20.06.18]

---
+++ Circle01/get_next_line/get_next_line.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/02 17:28:09 by gipark            #+#    #+#             */
+/*   Updated: 2020/06/18 19:58:30 by gipark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	count;
+
+	count = 0;
+	while (s[count])
+		count++;
+	return (count);
+}
+
+int		get_new_line(int fd, char **str, char **line)
+{
+	int		len;
+	char	*tmp;
+
+	len = 0;
+	while (str[fd][len] != '\n' && str[fd][len] != '\0')
+		len++;
+	if (str[fd][len] == '\n')
+	{
+		*line = ft_substr(str[fd], 0, len);
+		tmp = ft_strdup(&(str[fd])[len + 1]);
+		ft_strdel(&str[fd]);
+		str[fd] = tmp;
+		if (str[fd][0] == '\0')
+			ft_strdel(&str[fd]);
+		return (1);
+	}
+	else
+	{
+		*line = ft_strdup(str[fd]);
+		ft_strdel(&str[fd]);
+		return (0);
+	}
+}
+
+int		result_value(int rd_size, int fd, char **str, char **line)
+{
+	if (rd_size < 0)
+		return (-1);
+	else if (rd_size == 0 && str[fd] == '\0')
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	else
+		return (get_new_line(fd, str, line));
+}
+
+int		get_next_line(int fd, char **line)
+{
+	char		*buf;
+	static char	*str[OPEN_MAX];
+	char		*tmp;
+	int			rd_size;
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0 ||
+			!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while (0 < (rd_size = read(fd, buf, BUFFER_SIZE)))
+	{
+		buf[rd_size] = '\0';
+		if (str[fd] == '\0')
+			str[fd] = ft_strdup(buf);
+		else
+		{
+			tmp = ft_strjoin(str[fd], buf);
+			ft_strdel(&str[fd]);
+			str[fd] = tmp;
+		}
+		if (ft_strchr(str[fd], '\n'))
+			break ;
+	}
+	free(buf);
+	return (result_value(rd_size, fd, str, line));
+}
---
+++ Circle01/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/18 20:00:36 by gipark            #+#    #+#             */
+/*   Updated: 2020/06/18 20:00:42 by gipark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <limits.h>
+
+int		get_next_line(int fd, char **line);
+
+size_t	ft_strlen(const char *s);
+void	ft_strdel(char **s);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
