fbb8e984a23834762a9a4b395229e2fc4a02d393
gipark <gipark@student.42seoul.kr>
2020-09-11 11:29:58 +0900

    Code ft_printf.c ft_printf.h [20.09.11]

--- Circle01/ft_printf/includes/ft_printf.h
+++ Circle01/ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 05:20:53 by gipark            #+#    #+#             */
-/*   Updated: 2020/09/11 09:22:25 by gipark           ###   ########.fr       */
+/*   Updated: 2020/09/11 11:06:49 by gipark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,60 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
+# include "libft/libft.h"
 
+# define FLAGS		    "-+#0 "
+# define FLAGS_ALL 	    "-+#0 *.0123456789lh"
+# define CONVERSIONS    "cspdiuxX%nfge"
+
+# define DIGITS         "0123456789"
+# define HEX_LOW         "0123456789abcdef"
+# define HEX_UPP         "0123456789ABCDEF"
+
+
+typedef struct s_info
+{
+    char					set[20];
+	char					spec_c;
+	char					padd_c;
+	char					minus;
+	char					plus;
+	char					space;
+	char					hash;
+	int						width;
+	char					point;
+	int						precision;
+	char					length;
+	char					number_zero;
+	char					sign;
+	unsigned long long int	ulli;
+	long long int			lli;
+	char					*str;
+	size_t					strlen;
+}               t_info
+
+/*
+**  FILE: ft_printf.c
+*/
+int				ft_printf(const char *format, ...);
+
+/*
+**  FILE: ft_printf_cs.c
+*/
+void			print_percent(int *len, t_info inf);
+void			print_char(int *len, t_info inf, char c);
+void			print_string(int *len, t_info inf, char *s);
+
+/*
+**  FILE: ft_printf_iduxp.c
+*/
+void			print_decimal(int *len, t_info inf, va_list ap);
+void			print_hexa(int *len, t_info inf, va_list ap);
+void			print_pointer(int *len, t_info inf, unsigned long int p);
+
+/*
+**  FILE: ft_printf_pre.c
+*/
+void			print_flags(int *len, t_info inf);
 
 #endif
--- Circle01/ft_printf/srcs/ft_printf.c
+++ Circle01/ft_printf/srcs/ft_printf.c
@@ -6,14 +6,132 @@
 /*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 03:37:22 by gipark            #+#    #+#             */
-/*   Updated: 2020/09/08 12:50:50 by gipark           ###   ########.fr       */
+/*   Updated: 2020/09/11 11:28:18 by gipark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *format, ...)
+static void 	print_conversions(va_list ap, int *len, t_info inf)
+{
+	int		*pn;
+
+	if (inf.spec_c == '%')
+		print_percent(len, inf);
+	if (inf.spec_c == 'c')
+		print_char(len, inf, va_arg(ap, int));
+	if (inf.spec_c == 's')
+		print_string(len, inf, va_arg(ap, char *));
+	if (inf.spec_c == 'p')
+		print_pointer(len, inf, va_arg(ap, unsigned long int));
+	if (inf.spec_c == 'i' || inf.spec_c == 'd' || inf.spec_c == 'u')
+		print_decimal(len, inf, ap);
+	if (inf.spec_c == 'x' || inf.spec_c == 'X')
+		print_hexa(len, inf, ap);
+	if (inf.spec_c == 'n' && (p = va_arg(ap, int *)))
+		*pn = *len;
+}
+
+static t_info 	set_asterisk(va_list ap, t_info inf, int *j)
+{
+	int value;
+
+	(*j)++;
+	value = va_arg(ap, int);
+	if (inf.point == 0)
+	{
+		inf.width = (value >= 0) ? value : -value;
+		inf.padd_c = (value >= 0) ? inf.padd_c : ' ';
+		inf.minus = (value >= 0) ? inf.minus : 1;
+	}
+	if (inf.point == 1)
+	{
+		if (value >= 0)
+			inf.precision = value;
+		else
+			inf.point = 0;
+	}
+	return (inf);
+}
+
+static t_info 	prepare_flags(va_list ap, t_info inf)
+{
+	int		j;
+
+	j = 0;
+	while (inf.set[j] != '\0' && ft_strchr_v2(FLAGS, inf.set[j]))
+	{
+		if (inf.set[j++] == '\0')
+			inf.padd_c = '0';
+	}
+	if (inf.set[j] == '*')
+		inf = set_asterisk(ap, inf, &j);
+	while (inf.set[j] != '\0' && ft_strchr_v2(DIGITS, inf.set[j]))
+		inf.width = 10 * inf.width + inf.set[j++] - '0';
+	if (inf.set[j] == '.')
+	{
+		inf.point = 1;
+		if (inf.set[++j] == '*')
+			inf = set_asterisk(ap, inf, &j);
+		while (inf.set[j] != '\0' && ft_strchr_v2(DIGITS, inf.set[j]))
+			inf.precision = 10 * inf.precision + inf.set[j++] - '0';
+	}
+	while (inf.set[j] != '\0' && ft_strchr_v2("l", inf.set[j++]))
+		inf.length++;
+	while (inf.set[j] != '\0' && ft_strchr_v2("h", inf.set[j++]))
+		inf.length--;
+	return (inf);
+}
+
+static void		prepare_info(va_list ap, const char *format, int *len, int *i)
+{
+	t_info	inf;
+	int		j;
+
+	j = 0;
+	while (ft_strchr_v2(infAGS_ALL, format[*i]) && j < 19)
+		inf.set[j++] = format[(*i)++];
+	inf.set[j] = '\0';
+	if (ft_strchr_v2(CONVERSIONS, format[*i]))
+	{
+		inf.spec_c = format[(*i)++];
+		inf.minus = ft_strchr_v2(inf.set, '-');
+		inf.plus = ft_strchr_v2(inf.set, '+');
+		inf.space = ft_strchr_v2(inf.set, ' ');
+		inf.hash = ft_strchr_v2(inf.set, '#');
+		inf.width = 0;
+		inf.point = 0;
+		inf.precision = 0;
+		inf.length = 0;
+		inf.pad_c = ' ';
+		inf = prepare_flags(ap, inf);
+		print_conversions(ap, len, inf);
+	}
+	else
+		(*len) = -1;
+}
+
+int				ft_printf(const char *format, ...)
 {
 	va_list ap;
-	
+	int		len;
+	int		i;
+
+	va_start(ap, format);
+	len = 0;
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+			ft_putchar_len(format[i++], &len);
+		else
+		{
+			i++;
+			prepare_info(ap, format, &len, &i);
+			if (len == -1)
+				return (-1);
+		}
+	}
+	va_end(ap);
+	return (len);
 }
