abb031acddb35ab5747ab614f7b5fbdd771a9b3b
gipark <gipark@student.42seoul.kr>
2020-09-11 09:23:24 +0900

    Code ft_printf.c ft_printf.h Makefile [20.09.11]

--- Circle01/ft_printf/includes/ft_printf.h
+++ Circle01/ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 05:20:53 by gipark            #+#    #+#             */
-/*   Updated: 2020/09/08 11:52:02 by gipark           ###   ########.fr       */
+/*   Updated: 2020/09/11 09:22:25 by gipark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
-# include <stdint.h>
 
-/*
-** FORMAT SPECIFIER STRUCT
-*/
-typedef struct s_flags
-{
-	char					set[20];
-	char					spec_c;
-	char					padd_c;
-	char					minus;
-	char					plus;
-	char					space;
-	char					hash;
-	char					hash;
-	int						width;
-	char					point;
-	int						precision;
-	char					length;
-	char					print_n0;
-	char					sign;
-	unsigned long long int	ulli;
-	long long int			lli;
-}
 
 #endif
--- Circle01/ft_printf/srcs/ft_printf.c
+++ Circle01/ft_printf/srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 03:37:22 by gipark            #+#    #+#             */
-/*   Updated: 2020/09/08 10:30:34 by gipark           ###   ########.fr       */
+/*   Updated: 2020/09/08 12:50:50 by gipark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,5 @@
 int	ft_printf(const char *format, ...)
 {
 	va_list ap;
-	int		len;
-	int		i;
-
-	va_start(ap, format);
-	len = 0;
-	i = 0;
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-			ft_putchar_len(format[i++], &len);
-		else
-		{
-			i++;
-		
-		}
-	}
+	
 }
