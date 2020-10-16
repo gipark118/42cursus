17deaf2f847b5c9b03a36c7e9c503c0fd37a94e9
gipark <gipark@student.42seoul.kr>
2020-10-17 03:22:17 +0900

    Modify mysql query [20.10.17]

--- Circle02/ft_server/srcs/mysql_config.sql
+++ Circle02/ft_server/srcs/mysql_config.sql
@@ -1,6 +1,6 @@
 use mysql;
 flush privileges;
-CREATE DATABASE IF NOT EXISTS wordpress;
+create database if not exists wordpress;
 create user if not exists 'gipark'@'localhost' identified by '1201';
 grant all privileges on wordpress.* to 'gipark'@'localhost';
 use wordpress;
