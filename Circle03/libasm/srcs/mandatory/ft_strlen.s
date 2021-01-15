8e6b54db884b286ae04baf7d0c19b7864c99258d
gipark <gipark@student.42seoul.kr>
2021-01-15 11:10:53 +0900

    Modify ft_strlen.s [20.01.15]

--- Circle03/libasm/srcs/mandatory/ft_strlen.s
+++ Circle03/libasm/srcs/mandatory/ft_strlen.s
@@ -3,13 +3,13 @@ section .text
 
 _ft_strlen:
 	mov	rax, 0		; rax = 0
-	jmp loop		; jump to loop
+	jmp _loop		; jump to loop
 
-loop:
+_loop:
 	cmp BYTE [rdi + rax], 0			; rdi, rdi + rax, rdi + rax + rax... subtract 0
-	je	exit					; if [rcx + rax] - 0 equals 0, jump to exit
+	je	_exit					; if [rcx + rax] - 0 equals 0, jump to exit
 	inc	rax						; rax++
-	jmp loop					; jump to loop again
+	jmp _loop					; jump to loop again
 
-exit:
+_exit:
 	ret			; return rax
